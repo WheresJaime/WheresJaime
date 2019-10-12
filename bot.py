88ea6b5063d03e553283e1bb37d9384653d5b9fe
colten <colten@sullivent.net>
2019-10-11 23:57:27 -0700

    Added reply choice tree, started error handling for block

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import xml.etree.ElementTree as ET
 import tweepy
 from random import choice
+from datetime import datetime
+import string
 
 # 130, 200, 230
 
 class Bot:
   def __init__(self):
+    self.LAST = datetime(year=2017, month=1, day=17, hour=12)
     conf = ET.parse('OAuth.xmls')
     confRoot = conf.getroot()
 
@@ -30,7 +33,24 @@ class Bot:
     Check if target account has posted, update the cache, fire reply if needed
     :return: none
     """
-    pass
+    tree = ET.parse('cache.xml')
+
+    # TODO: Check my last post time to avoid spamming
+    # TODO: This, but well
+    try:
+      currentId = self.api.user_timeline(id="herrerabeutler", count=1)[0].id_str
+      #currentId = self.api.user_timeline(id="littelbro14")[0].id_str
+    except Exception as e:
+      mode = tree.find('.//mode').text
+      print(e)
+
+    cachedId = tree.find('.//lastTargetPost').text
+    if currentId != cachedId:
+      tree.find('.//lastTargetPost').text = currentId
+      tree.write('cache.xml')
+      # TODO: separate out and generalize zfill to include hero and reply
+      postId = '90' + str(choice(range(8)) + 1)
+      self._post(postId)
 
   def post(self):
     """
@@ -38,24 +58,30 @@ class Bot:
     TODO: handle timing internally, possibly as a daemon
     :return: none
     """
-    root = ET.parse('strings.xml').getroot()
     div = choice([0,1,2])  # Probably a silly way to do this
     spec = choice(range(32))
 
     if div != 0:  # 1/3 ought to be default
       # post default
       postId = "000"
-      pass
     else:  # 2/3 ought to be non-default
       # post non-default, chosen by spec
+      # TODO: separate out and generalize zfill to include hero and reply
       postId = str(spec+1).zfill(3)
 
-    # TODO: Separate out into own method and generalize for replies and heroes also
-    for child in root:
-      if child.attrib["id"] == postId:
-        print(child.text)  # TODO: go live
-        break
+    self._post(postId)
 
+  def _post(self, id):
+    root = ET.parse('strings.xml').getroot()
+    days = (datetime.now() - self.LAST).days
+
+    for child in root:
+      if child.attrib["id"] == id:
+        text = child.text.replace("X", str(days))
+
+        # TODO: Go live
+        #self.api.update_status(text)
+        break
 
 def main(op):
   """
@@ -75,4 +101,4 @@ def main(op):
 
 
 if __name__ == "__main__":
-  main(1)
+  main(0)
